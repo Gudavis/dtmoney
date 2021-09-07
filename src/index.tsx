462282480bd96f9c39d622cc8fa8919dfbfd9bfd
Gustavo Davis <gustavo.davis@outlook.com>
2021-09-07 13:00:54 -0300

    created new transaction modal and configure mirage

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import App from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento de Site',
+          category: 'Dev',
+          type: 'deposit',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          category: 'Casa',
+          type: 'withdraw',
+          amount: 1000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        }
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [{
-        id: 1,
-        title: 'Transaction 1',
-        amount: 500,
-        category: 'Food',
-        date: new Date()
-      }]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
     })
   }
 })
